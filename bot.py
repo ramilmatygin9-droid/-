da3547a142f5e66dfa6b2159a1b269259f5d7ec2
GLITCH-WIN <ramilmatygin9@gmail.com>
2026-07-03 23:29:39 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,36 +1,40 @@
-import logging
+import os
 import asyncio
+import logging
+import random
+import string
+import aiohttp
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
+from aiogram.types import BotCommand, BotCommandScopeDefault
 
-# Включаем логирование
 logging.basicConfig(level=logging.INFO)
 
-# --- КОНФИГУРАЦИЯ ---
-# Вставь сюда токен, который тебе выдал @BotFather
-BOT_TOKEN = "8156857401:AAFkS4GaCYxGEyFyAwgqsqyah-d9PPNHeH0"
-
-# Твой текст автоответа
-REPLY_TEXT = "Привет! Я сейчас занят либо меня сбил камаз 🚛 Мой актуальный юзернейм: @Z_L_0_l"
-
-# Инициализация бота и диспетчера
-bot = Bot(token=BOT_TOKEN)
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# Обработка команды /start
+user_mails = {}
+
+def generate_password(length=12):
+    chars = string.ascii_letters + string.digits
+    return ''.join(random.choice(chars) for _ in range(length))
+
+def get_main_keyboard():
+    kb = [
+        [types.KeyboardButton(text="ПОШЕЛ НАХУЙ")]
+
 @dp.message(Command("start"))
 async def start_cmd(message: types.Message):
-    await message.answer(REPLY_TEXT)
-
-# Обработка абсолютно любых других текстовых сообщений
-@dp.message(F.text)
-async def all_messages(message: types.Message):
-    await message.answer(REPLY_TEXT)
-
-# Главная функция запуска
+    await message.answer(
+        "**Привет! Я сейчас занят либо меня сбил камаз 🚛 Мой актуальный юзернейм: @Z_L_0_l",
+        reply_markup=get_main_keyboard(),
+        parse_mode="Markdown"
+    
 async def main():
-    print("🤖 Бот-автоответчик по токену запущен!")
-    await dp.start_polling(bot)
+    await bot.set_my_commands([BotCommand(command="/start", description="Запуск")], scope=BotCommandScopeDefault())
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main())
+ вот пример еще
